6e7eebf8a7a43066beb559de14d695913ca16ad3
alejandrogualdron <alejandrogualdron@users.noreply.github.com>
2021-01-20 15:47:51 -0500

    Version BD

--- app/src/main/java/project/main/Registrar.java
+++ app/src/main/java/project/main/Registrar.java
@@ -1,5 +1,6 @@
 package project.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,32 +11,47 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Registrar extends AppCompatActivity implements View.OnClickListener {
 
     EditText us,pas,nom,ap,dir,ced,edad;
     RadioButton genero;
     Button reg,can;
-    daoUsuario dao;
+
+    String generoS;
+    String usu="";
+    String pass="";
+    String nomb="";
+    String ape="";
+    String dire="";
+    String cedu="";
+    String ed="";
+
+    FirebaseAuth mAuth;
+    DatabaseReference nDatabase;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registrar);
-        us=(EditText)findViewById(R.id.RegUser);
-        pas=(EditText)findViewById(R.id.RegPass);
-        nom=(EditText)findViewById(R.id.RegNombre);
-        ap=(EditText)findViewById(R.id.RegApellido);
-        dir=(EditText)findViewById(R.id.RegDireccion);
-        ced=(EditText)findViewById(R.id.RegCedula);
-        edad=(EditText)findViewById(R.id.RegEdad);
 
+        mAuth=FirebaseAuth.getInstance();
+        nDatabase=FirebaseDatabase.getInstance().getReference();
 
         genero=(RadioButton)findViewById(R.id.RegRadbtnH);
         reg=(Button)findViewById(R.id.btnRegRegistrar);
         can=(Button)findViewById(R.id.btnRegCancelar);
         reg.setOnClickListener(this);
         can.setOnClickListener(this);
-        //dao=new daoUsuario(this);
     }
 
 
@@ -43,28 +59,39 @@ public class Registrar extends AppCompatActivity implements View.OnClickListener
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btnRegRegistrar:
-                Usuario u=new Usuario();
-                u.setUsuario(us.getText().toString());
-                u.setPassword(pas.getText().toString());
-                u.setNombre(nom.getText().toString());
-                u.setApellido(ap.getText().toString());
-                u.setDireccion(dir.getText().toString());
-                u.setEdad(edad.getText().toString());
-                u.setCedula(ced.getText().toString());
 
-                if (genero.isChecked()==true){
-                    u.setGenero("Hombre");
 
-                }else{
-                    u.setGenero("Mujer");
+                //Firebase
+
+                usu=(us.getText().toString());
+                pass=(pas.getText().toString());
+                nomb=(nom.getText().toString());
+                ape=(ap.getText().toString());
+                dire=(dir.getText().toString());
+                ed=(edad.getText().toString());
+                cedu=(ced.getText().toString());
+
+                if (usu.isEmpty()&&pass.isEmpty()&&nomb.isEmpty()&&
+                        ape.isEmpty()&&dire.isEmpty()&&ed.isEmpty()&&cedu.isEmpty()){
+                    if (pass.length()>=6){
+                        registerUser();
+                    }else {
+                        Toast.makeText(this,"La contraseña debe tener 6 caracteres", Toast.LENGTH_LONG).show();
+                    }
+
+                }else {
+                    Toast.makeText(this,"Debe completar la información", Toast.LENGTH_LONG).show();
                 }
 
-                /*if (u.isNull()){
-                    Toast.makeText(this,"Errorr: Campos vacios", Toast.LENGTH_LONG).show();
-                }else {
-                    Intent i=new Intent(Registrar.this,MainActivity.class);
-                    startActivity(i);
-                }*/
+                //Obtener dato radioButton
+                if (genero.isChecked()==true){
+                    generoS="Hombre";
+
+                }else{
+                    generoS="Mujer";
+                }
+
+
                 Intent b=new Intent(Registrar.this,MainActivity.class);
                 startActivity(b);
                 break;
@@ -74,6 +101,18 @@ public class Registrar extends AppCompatActivity implements View.OnClickListener
                 startActivity(i);
                 break;
         }
+    }
+
+    private void registerUser() {
+        mAuth.createUserWithEmailAndPassword(usu,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+
+                }else{
+                    Toast.makeText(Registrar.this,"No se pudo registrar este usuario", Toast.LENGTH_LONG).show();
+                }
+            }});
     }
 
 }
