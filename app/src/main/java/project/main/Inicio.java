11326565787e64d95bf534282ba324a787439462
alejandrogualdron <alejandrogualdron@users.noreply.github.com>
2021-01-22 16:54:15 -0500

    interfaz maps

--- app/src/main/java/project/main/Inicio.java
+++ app/src/main/java/project/main/Inicio.java
@@ -2,8 +2,16 @@ package project.main;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,35 +24,39 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class Inicio extends AppCompatActivity implements View.OnClickListener{
+public class Inicio extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnEditar,btnEliminar,btnRepSintomas,btnSalir;
+    private Button btnEditar, btnEliminar, btnRepSintomas, btnSalir;
     private FirebaseAuth nAuth;
     private TextView textViewName;
     private DatabaseReference nDatabase;
+
+    double latitud=0;
+    double longitud=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicio);
 
-        nAuth=FirebaseAuth.getInstance();
-        nDatabase= FirebaseDatabase.getInstance().getReference();
+        nAuth = FirebaseAuth.getInstance();
+        nDatabase = FirebaseDatabase.getInstance().getReference();
 
-        btnRepSintomas=(Button)findViewById(R.id.btnReportar);
+        btnRepSintomas = (Button) findViewById(R.id.btnReportar);
         btnRepSintomas.setOnClickListener(this);
 
-        btnEliminar=(Button)findViewById(R.id.btnEliminar);
+        btnEliminar = (Button) findViewById(R.id.btnEliminar);
         btnEliminar.setOnClickListener(this);
 
-        btnSalir=(Button)findViewById(R.id.btnSalir);
+        btnSalir = (Button) findViewById(R.id.btnSalir);
         btnSalir.setOnClickListener(this);
 
-        btnEditar=(Button)findViewById(R.id.btnEditar);
+        btnEditar = (Button) findViewById(R.id.btnEditar);
         btnEditar.setOnClickListener(this);
 
-        textViewName=(TextView) findViewById(R.id.nombreUsuario);
+        textViewName = (TextView) findViewById(R.id.nombreUsuario);
         textViewName.setOnClickListener(this);
+
 
         obtenerInfoDB();
 
@@ -52,50 +64,71 @@ public class Inicio extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btnEditar:
-                Intent i1=new Intent(Inicio.this,Editar.class);
+                Intent i1 = new Intent(Inicio.this, Editar.class);
                 startActivity(i1);
                 break;
 
             case R.id.btnEliminar:
-                Intent i3=new Intent(Inicio.this,Ubicacion.class);
+
+                Intent i3 = new Intent(Inicio.this, Ubicacion.class);
                 startActivity(i3);
                 break;
 
             case R.id.btnReportar:
-                Intent i2=new Intent(Inicio.this,ReprteDeSintomas.class);
+                Intent i2 = new Intent(Inicio.this, ReprteDeSintomas.class);
                 startActivity(i2);
                 break;
 
             case R.id.btnSalir:
                 nAuth.signOut();
-                startActivity(new Intent(Inicio.this,MainActivity.class));
+                startActivity(new Intent(Inicio.this, MainActivity.class));
                 finish();
                 break;
         }
     }
 
-    private void obtenerInfoDB(){
-        String id= nAuth.getCurrentUser().getUid();
+    private void obtenerInfoDB() {
+        String id = nAuth.getCurrentUser().getUid();
         nDatabase.child("Users").child(id).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()){
+                if (snapshot.exists()) {
 
                     //Obtiene valores de la base de datos
-                    String name= snapshot.child("nombre").getValue().toString();
-                    String cedula= snapshot.child("cedula").getValue().toString();
-                    String estado= snapshot.child("estado").getValue().toString();
+                    String name = snapshot.child("nombre").getValue().toString();
+                    String cedula = snapshot.child("cedula").getValue().toString();
+                    String estado = snapshot.child("estado").getValue().toString();
                     textViewName.setText(name);
                 }
 
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
         });
     }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+
+
+
+
+
 }
--- app/src/main/java/project/main/Ubicacion.java
+++ app/src/main/java/project/main/Ubicacion.java
@@ -6,6 +6,7 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -14,16 +15,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Ubicacion extends AppCompatActivity {
 
     Button btnGPS;
     TextView tvUbicacion;
+    double la,lo;
+    Maps l;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ubicacion);
+        l=new Maps();
 
         tvUbicacion = (TextView)findViewById(R.id.tvUbicacion);
         btnGPS = (Button)findViewById(R.id.button);
@@ -32,13 +37,17 @@ public class Ubicacion extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Acquire a reference to the system Location Manager
-                LocationManager locationManager = (LocationManager) Ubicacion.this.getSystemService(Context.LOCATION_SERVICE);
+                LocationManager locationManager = (LocationManager)
+                        Ubicacion.this.getSystemService(Context.LOCATION_SERVICE);
 
-// Define a listener that responds to location updates
+                // Define a listener that responds to location updates
                 LocationListener locationListener = new LocationListener() {
                     public void onLocationChanged(Location location) {
                         // Called when a new location is found by the network location provider.
-                        tvUbicacion.setText("" + location.getLatitude()+" "+ location.getLongitude());
+                        la=location.getLatitude();
+                        lo=location.getLongitude();
+                        tvUbicacion.setText("" + la+" "+ lo);
+
                     }
 
                     public void onStatusChanged(String provider, int status, Bundle extras) {}
@@ -48,10 +57,15 @@ public class Ubicacion extends AppCompatActivity {
                     public void onProviderDisabled(String provider) {}
                 };
 
-// Register the listener with the Location Manager to receive location updates
+                // Register the listener with the Location Manager to receive location updates
                 int permissionCheck = ContextCompat.checkSelfPermission(Ubicacion.this,
                         Manifest.permission.ACCESS_FINE_LOCATION);
                 locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
+                l.coordenadas(la,lo);
+              //  Intent i2 = new Intent(Ubicacion.this, Maps.class);
+             //   startActivity(i2);
+
             }
 
 
@@ -69,5 +83,22 @@ public class Ubicacion extends AppCompatActivity {
         }
 
 
+    }
+
+
+    public double getLa() {
+        return la;
+    }
+
+    public void setLa(double la) {
+        this.la = la;
+    }
+
+    public double getLo() {
+        return lo;
+    }
+
+    public void setLo(double lo) {
+        this.lo = lo;
     }
 }
